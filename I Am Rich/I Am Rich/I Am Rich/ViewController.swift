dc2400c90bddb6a8a1b2dae0dd679b62e8bbda5d
Taha Babar <tahababar70@gmail.com>
2020-08-27 18:55:52 +0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Taha Babar on 8/10/20.
+//  Copyright © 2020 Taha Babar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
